668c4b2ae7745abb18fce247f763f47bb5afe389
Ambiguous666 <2087791511@qq.com>
2023-01-30 19:11:14 +0800

    add table-demo

---
+++ vue3/table-demo/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <h1>App 根组件</h1>
+    <my-table :data="goodslist">
+      <template v-slot:header>
+        <th>序号</th>
+        <th>商品名称</th>
+        <th>价格</th>
+        <th>标签</th>
+        <th>操作</th>
+      </template>
+
+      <template v-slot:body="{ row, index} ">
+        <td>{{ index +1 }}</td>
+        <td>{{ row.goods_name }}</td>
+        <td>￥{{ row.goods_price }}</td>
+        <td>
+          <input type="text" class="form-control form-control-sm" v-if="row.inputVisible" v-focus 
+            v-model.trim="row.inputValue" @blur="onInputConfirm(row)" @keyup.enter="onInputConfirm(row)"
+              @keyup.esc="row.inputValue=''"/>
+          <button type="button" class="btn btn-primary btn-sm" v-else @click="row.inputVisible = true">+Tag</button>
+          <span class="badge badge-warning ml-2" v-for="item in row.tags" :key="item">{{ item }}</span>
+        </td>
+        <td>
+          <button type="button" class="btn btn-danger btn-sm" @click="onRemove(row.id)">删除</button>
+        </td>
+      </template>
+    </my-table>
+  </div>
+</template>
+
+<script>
+import MyTable from './components/my-table/MyTable.vue'
+export default {
+  name: 'MyApp',
+  data() {
+    return {
+      goodslist: [],
+    }
+  },
+  created() {
+    this.getGoodsList()
+  },
+  methods: {
+    async getGoodsList() {
+      const {data:res} = await this.$http.get('/api/goods')
+      if(res.status !== 0) return console.log('get goods list failed')
+      this.goodslist = res.data
+    },
+    onRemove(id){
+      this.goodslist = this.goodslist.filter(x=>x.id!==id)
+    },
+    onInputConfirm(row){
+      const val = row.inputValue
+      row.inputValue = ''
+      row.inputVisible = false
+      if(!val||row.tags.indexOf(val)!==-1) return
+      row.tags.push(val)
+    },
+  },
+  directives: {
+    focus(el) {
+      el.focus()
+    },
+  },
+  components: {
+    MyTable,
+  },
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ vue3/table-demo/src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import './assets/css/bootstrap.css'
+
+import axios from 'axios'
+
+const app = createApp(App)
+
+app.config.globalProperties.$http = axios
+
+axios.defaults.baseURL='https://www.escook.cn'
+
+
+
+app.mount('#app')
